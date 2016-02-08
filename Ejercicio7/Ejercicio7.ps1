b7094f053f6c5705d5e68b5275a495676fd962cc
Greenleave IT <seebogado@hotmail.com>
2016-02-08 13:53:29 -0300

    Update Ejercicio7.ps1
    Se agrega algo magico de codigo .net C#

--- Ejercicio7/Ejercicio7.ps1
+++ Ejercicio7/Ejercicio7.ps1
@@ -42,9 +42,144 @@ Param
     $Operacion
 )
 
+
+
+Add-Type -Language CSharp @"
+class matrizWrapper
+    {
+
+        int cantidadColumnas;
+        int cantidadFilas;
+        System.Collections.ArrayList matriz;
+
+        matrizWrapper(System.Collections.ArrayList lineaArchivo)
+        {
+            this.matriz = new System.Collections.ArrayList();
+            if (!validarMatriz(lineaArchivo))
+            {
+                System.Console.Error.Write("Error en la cantidad de columnas");
+                System.Console.Error.Close();
+            }
+             
+        }
+
+        private matrizWrapper()
+        {
+            this.matriz = new System.Collections.ArrayList();
+            this.cantidadColumnas = this.cantidadFilas = 0;
+        }
+
+        public matrizWrapper transponer(matrizWrapper otraMatriz)
+        {
+            matrizWrapper matrizAuxiliar = new matrizWrapper();
+            matrizAuxiliar.cantidadFilas = this.cantidadColumnas;
+            matrizAuxiliar.cantidadColumnas = this.cantidadFilas;
+            for(int i=0; i<this.cantidadColumnas; i++)
+            {
+                for (int j = 0; j < this.cantidadFilas; j++)
+                    matrizAuxiliar.matriz.Add(this.matriz[i + j * this.cantidadColumnas]);
+                    
+            }
+            return matrizAuxiliar;
+        }
+        public matrizWrapper restar(matrizWrapper otraMatriz)
+        {
+            if (this.cantidadColumnas != otraMatriz.cantidadColumnas || this.cantidadFilas != otraMatriz.cantidadFilas)
+            {
+                System.Console.Error.Write("Error, las matrices deben ser iguales tanto en filas como en columnas");
+            }
+            matrizWrapper matrizResultado = new matrizWrapper();
+            matrizResultado.cantidadFilas = this.cantidadFilas;
+            matrizResultado.cantidadColumnas = this.cantidadColumnas;
+            for(int i =0; i < this.cantidadColumnas * this.cantidadFilas; i++)
+            {
+                matrizResultado.matriz.Add((double)this.matriz[i] - (double)otraMatriz.matriz[i]);
+            }
+            
+            return matrizResultado;
+        }
+
+        public matrizWrapper sumar(matrizWrapper otraMatriz)
+        {
+            if (this.cantidadColumnas != otraMatriz.cantidadColumnas || this.cantidadFilas != otraMatriz.cantidadFilas)
+            {
+                System.Console.Error.Write("Error, las matrices deben ser iguales tanto en filas como en columnas");
+            }
+            matrizWrapper matrizResultado = new matrizWrapper();
+            matrizResultado.cantidadFilas = this.cantidadFilas;
+            matrizResultado.cantidadColumnas = this.cantidadColumnas;
+            for (int i = 0; i < this.cantidadColumnas * this.cantidadFilas; i++)
+            {
+                matrizResultado.matriz.Add((double)this.matriz[i] + (double)otraMatriz.matriz[i]);
+            }
+
+            return matrizResultado;
+        }
+
+        private bool validarMatriz(System.Collections.ArrayList linea)
+        {
+            this.cantidadFilas = linea.Count;
+            this.cantidadColumnas = ((string) linea[0]).Split(';').Length;
+            for (int i=0; i<this.cantidadFilas;i++)
+            {
+                string[] fila = ((string)linea[i]).Split(';');
+                if (fila.Length != this.cantidadColumnas)
+                {
+                    return false;
+                }
+                else
+                {
+                    //añado los elementos de la fila presente
+                    for (int j = 0; j < this.cantidadColumnas; j++)
+                    {
+                        //Lo convierto en un double
+                        try
+                        {
+                            matriz.Add(Convert.ToDouble(fila[j]));
+                        }
+                        catch(FormatException)
+                        {
+                            System.Console.Error.Write("Error, el contenido de los elementos deben ser numeros reales");
+                            System.Console.Error.Close();
+                        }
+                        
+                    }
+                }
+            }
+            return true;
+        }
+
+        public void mostrarMatriz()
+        {
+            for(int i=0; i<this.cantidadFilas; i++)
+            {
+                string linea = "";
+                for(int j=0; j<this.cantidadColumnas; j++)
+                {
+                    linea += this.matriz[i * cantidadColumnas + j] + "|";
+                }
+                System.Console.WriteLine(linea);
+                
+            }
+        }
+
+    }
+"@;
+
+function transponerMatriz($cantidadColumnas, $cantidadFilas, $array){
+    for($j=0; $j -lt $cantidadColumnas; $j++){
+        $linea=""
+        for($i=0; $i -lt $cantidadFilas; $i++){
+         $linea += ( $array[$j+$cantidadColumnas*$i] -as [string] ) + " | "
+        }
+        echo $linea
+    }
+
+}
+
+
 function abrirYCargarArchivo(){
-    Write-Debug "LLega a abrir el archivo"
-    $delimitadorFilas=";;"
+    $delimitadorFilas=';;'
     $delimitadorColumnas=";"
     if( Test-Path $PathMatriz ){
         if(Test-Path $PathMatriz -PathType Container){
@@ -54,43 +189,56 @@ function abrirYCargarArchivo(){
         $file = Get-Content $PathMatriz
         switch($file.count){
             1{
-                Write-Host "llega al switch del archivo"
-                $arrayAux=$file.Split($delimitadorFilas)
-                [string [][]]$matriz= new String[][];
-                echo $arrayAux.Count
 
-                $cantidadColumnas=0;
-                $contColumnas=0;
-                $cantidadFilas=0;
+                if($Operacion.CompareTo("trans") -ne 0 ){
+                    Write-Error "La matriz debe tener 2 elementos para realizar la operacion $Operacion."
+                    exit
+                }
 
-                foreach ($elemento in $arrayAux){
-                    ##La cantidad de letras del elemento es distinta de cero
-                    ##Tengo agregarlo a la matriz y sumar 1 a la cantidad de columnas
-                    if($elemento.Length -ne 0){
-                        $matriz[$cantidadFilas][$contColumnas]=$elemento;
-                        $contColumnas++;
+                $arrayAux=$file -split ";;"
+                $array=New-Object System.Collections.ArrayList
+                $cantidadFilas=$arrayAux.Length;
+                $cantidadColumnas=$arrayAux[1].Split(";").Length
+                for($i=0;$i -lt $arrayAux.Length; $i++){
+                    if($cantidadColumnas -ne $arrayAux[$i].Split(";").Count){
+                        Write-Error "La cantidad de columnas en la fila $i debe de ser igual al resto"
+                        exit
                     }
-                    else{
-
-                        if( $contColumnas -eq 0 ){
-                            Write-Error "La matriz no puede contener filas vacias"
+                    $element = $arrayAux[$i].Split(";")
+                    for($j=0; $j -lt $cantidadColumnas; $j++){
+                        try
+                        { 
+                           $array.Add([convert]::ToDouble(${element[$j]}))
+                        }
+                        catch [FormatException]
+                        {
+                            Write-Error “El archivo no debe contener letras”
                             exit
                         }
-                        else{
-                            if( $cantidadColumnas -eq 0 ){
-                                $cantidadColumnas=$contColumnas
-                            }
-                            if( $cantidadColumnas -ne $contColumnas ){
-                                Write-Error "La fila $cantidadFilas es desigual en la cantidad de columnas. Las columnas anteriores eran de $cantidadColumnas elementos y esta contiene $contColumnas"
-                            }
-                            $cantidadFilas++
-                        }
                     }
-                 }
+
+                }
+                transponerMatriz $cantidadColumnas $cantidadFilas $array
             }
             
             2{
-                Write-Host "Solo dos parametros"
+
+                $matriz1= New-Object Matriz
+                switch($Operacion){
+                "suma"
+                {
+                    
+                }
+                "resta"
+                {
+                    
+                }
+                "multi"
+                {
+                    
+                }
+                }
+
             }
             default{
                 Write-Error "Error el archivo debe contener dos o una linea."
@@ -101,10 +249,6 @@ function abrirYCargarArchivo(){
     else{
         Write-Error "El path no existe"
     }
-}
-
-function transponerMatriz(){
-    Write-Host "tranponer la matriz"
 }
 
 function discriminarOperacionBinaria(){
